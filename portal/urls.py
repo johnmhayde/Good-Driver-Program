208de1276683ffbd50228e86de57921dbddcf43b
John Michael Hayde <johnmichaelhayde@gmail.com>
2021-02-03 13:37:44 -0500

    Added basic Driver account creation form, created simple driver model

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,5 @@ from . import views
 # list of URL's for this app
 urlpatterns = [
     path('', views.home, name='portal-home'),
+	path('register/', views.register, name='register-user')
 ]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Model for Driver Table
+class DriverUser(models.Model):
+	username = models.CharField(max_length=20)
+	password = models.CharField(max_length=30)
