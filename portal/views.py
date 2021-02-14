e7e2bea6c1867cc62426fec12681281250111d8b
tjmorse <60407147+tjmorse@users.noreply.github.com>
2021-02-13 19:53:51 -0500

    Log in completed, /portal/home now redirects to the appropriate homepage depending on the type of user.

--- portal/views.py
+++ portal/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from users.models import Driver
+from users.models import Sponsor
+from users.models import GenericUser
+from django.shortcuts import redirect
 from django.contrib.auth.models import User
 
 # send user to homepage
 def home(request):
-	return render(request, 'portal/home.html')
+	user = request.user
+	gUser = GenericUser.objects.get(username=user.username)
+	userType = gUser.type
+	if userType == 'Driver':
+		response = redirect('driver-home')
+	elif userType == 'Sponsor':
+		response = redirect('sponsor-home')
+	else:
+		response = redirect('logout')
+	return response
+	#return render(request, 'portal/home.html')
 
 def register(request):
 	return render(request, 'portal/register.html')
@@ -20,4 +33,4 @@ def driver_home(request):
 	return render(request, 'portal/driver_home.html', data)
 
 def sponsor_home(request):
-	return render(request, 'portal/home.html')
+	return render(request, 'portal/sponsor_home.html')
