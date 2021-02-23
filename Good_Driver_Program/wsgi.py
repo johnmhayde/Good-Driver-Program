2e85ad3c74f95e2c135dc07aa81b39bd3952aafd
John Michael Hayde <johnmichaelhayde@gmail.com>
2021-02-22 22:39:55 -0500

    changed settings file

--- Good_Driver_Program/wsgi.py
+++ Good_Driver_Program/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Good_Driver_Program.settings')
 
-application = app = get_wsgi_application()
+application = get_wsgi_application()
