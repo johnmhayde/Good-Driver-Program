f25b5a0a8d590b314e98e4824a970a3dcf763625
John Michael Hayde <johnmichaelhayde@gmail.com>
2021-02-25 14:25:06 -0500

    restoring changes

--- Good_Driver_Program/settings.py
+++ Good_Driver_Program/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = 'Good_Driver_Program.wsgi.application'
 # 	        'NAME': BASE_DIR / 'db.sqlite3',
 # 	    }
 # 	}
-	DATABASES = {
-	    'default': {
-	        'ENGINE': 'django.db.backends.sqlite3',
-	        'NAME': BASE_DIR / 'db.sqlite3',
-	    }
+DATABASES = {
+	'default': {
+	    'ENGINE': 'django.db.backends.sqlite3',
+	    'NAME': BASE_DIR / 'db.sqlite3',
+	}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
