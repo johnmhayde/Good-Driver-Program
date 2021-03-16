9a1f0f561a5a3f03dac4322fe72d7798bf3fc037
John Michael Hayde <johnmichaelhayde@gmail.com>
2021-03-16 09:40:09 -0400

    added user login, logout, and failed login logs

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserLogin, UserLogout
 
-# Register your models here.
+class UserLoginDisplay(admin.ModelAdmin):
+	model = UserLogin
+	list_display = ('username', 'date', 'time', 'success')
+
+class UserLogoutDisplay(admin.ModelAdmin):
+	model = UserLogout
+	list_display = ('username', 'date', 'time')
+
+admin.site.register(UserLogin, UserLoginDisplay)
+admin.site.register(UserLogout, UserLogoutDisplay)
--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.dispatch import receiver
+from datetime import datetime
 
-# Create your models here.
+# table for logs of user login attempts
+class UserLogin(models.Model):
+	username = models.CharField(max_length=30)
+	date = models.DateField(auto_now_add=True)
+	time = models.TimeField(auto_now_add=True)
+	success = models.CharField(max_length=10, default="")
+
+class UserLogout(models.Model):
+	username = models.CharField(max_length=30)
+	date = models.DateField(auto_now_add=True)
+	time = models.TimeField(auto_now_add=True)
+
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):
+	UserLogin.objects.create(username=user.username, date = "Today", time = "", success="true")
+
+@receiver(user_login_failed)
+def user_login_failed_callback(sender, credentials, **kwargs):
+	UserLogin.objects.create(username=credentials.get('username', None), date = "Today", time = "", success="false")
+
+@receiver(user_logged_out)
+def user_logged_out_callback(sender, request, user, **kwargs):
+	UserLogout.objects.create(username=user.username, date = "Today", time = "")
