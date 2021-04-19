011240c8c316c3322aa38f72f279024c9ea5307e
John Michael Hayde <johnmichaelhayde@gmail.com>
2021-04-18 22:41:08 -0400

    clean settings file

--- Good_Driver_Program/settings.py
+++ Good_Driver_Program/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'c2z)o(scoexds@eh2me!sx7#o-j@y25x3olpi=f2mfb_ayql=y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['localhost','Good-Driver-Program-dev.us-east-1.elasticbeanstalk.com', '172.31.24.174', '127.0.0.1', 'localhost:8000']
+# '172.31.24.174',
+ALLOWED_HOSTS = ['localhost','Good-Driver-Program-dev.us-east-1.elasticbeanstalk.com', '127.0.0.1', 'localhost:8000']
 
 # Application definition
 
